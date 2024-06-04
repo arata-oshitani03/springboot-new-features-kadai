5550bf21383ef63b08673c704c4c47ac5e53adab
arata-oshitani03 <159011416+arata-oshitani03@users.noreply.github.com>
2024-06-04 21:23:32 +0900

    Delete src/target directory

--- src/target/classes/static/js/stripe.js
+++
@@ -1,8 +0,0 @@
-const stripe = Stripe('pk_test_51PAoUCP5oEoi7I0OVflSe0UYlxpFntwbtJYKw5YZBEoQuFDUjN0DOsJB5AbwdAGyRDq07vM3ToJikVsix9DcFOoK00RwxamQYX');
- const paymentButton = document.querySelector('#paymentButton');
- 
- paymentButton.addEventListener('click', () => {
-   stripe.redirectToCheckout({
-     sessionId: sessionId
-   })
- });
